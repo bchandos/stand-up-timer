7f5d6933abffe752aa82cedca82ebd3de7af40f5
bchandos <contact@billchandos.dev>
2022-07-30 13:21:54 -0700

    - user CheckMenuItem - window spawning menu item - separators - timer label in app indicator area

--- app-indicator.py
+++ app-indicator.py
@@ -1,74 +1,102 @@
+import datetime
 import os
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
-from gi.repository import Gtk as gtk, AppIndicator3 as appindicator
+from gi.repository import Gtk as gtk, AppIndicator3 as appindicator, GLib as glib
 
 from tracking import track_sitting, track_standing
 
 def main():
     indicator = appindicator.Indicator.new(
-        'customtray', 
+        'stand-up-timer', 
         f'{os.getcwd()}/icons/icon.png',
         appindicator.IndicatorCategory.APPLICATION_STATUS
     )
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(menu())
+    glib.timeout_add(1000, change_label, indicator)
     gtk.main()
 
 def menu():
     menu = gtk.Menu()
     
-    commands = [
+    menu_items = [
         {
-            'name': 'Stand Up',
+            'type': 'CheckMenuItem',
+            'label': 'Stand Up',
             'f': stand_up
         },
         {
-            'name': 'Sit Down',
+            'type': 'CheckMenuItem',
+            'label': 'Sit Down',
             'f': sit_down
         },
         {
-            'name': 'Exit Tray',
+            'type': 'SeparatorMenuItem',
+        },
+        {
+            'type': 'MenuItem',
+            'label': 'Get Stats',
+            'f': get_stats
+        },
+        {
+            'type': 'SeparatorMenuItem',
+        },
+        {
+            'type': 'MenuItem',
+            'label': 'Exit Tray',
             'f': quit_menu
         }
     ]
 
-    for command in commands:
-        c = gtk.MenuItem(label=command['name'])
-        c.connect('activate', command['f'])
+    for item in menu_items:
+        ME = getattr(gtk, item['type'])
+        if item.get('label'):
+            c = ME(label=item['label'])
+        else:
+            c = ME()
+        if item.get('f'):
+            c.connect('activate', item['f'])
         menu.append(c)
 
     menu.show_all()
     return menu
   
 def stand_up(menu_item):
-    track_standing()
-    menu = menu_item.get_parent()
-    set_checks(menu, menu_item)
-    # label = menu_item.get_label()
-    # menu_item.set_label(f'\u2713 {label}')
+    if menu_item.get_active():
+        print('Standing up.')
+        track_standing()
+        untoggle_others(menu_item)
 
 def sit_down(menu_item):
-    track_sitting()
+    if menu_item.get_active():
+        print('Sitting down.')
+        track_sitting()
+        untoggle_others(menu_item)
+
+def get_stats(menu_item):
+    w = gtk.Window(
+        title='Standing Stats',
+    )
+    w.set_default_size(500, 500)
+    w.set_destroy_with_parent(True)
+    w.show()
+
+def untoggle_others(menu_item):
     menu = menu_item.get_parent()
-    set_checks(menu, menu_item)
-    # label = menu_item.get_label()
-    # menu_item.set_label(f'\u2713 {label}')
+    children = menu.get_children()
+    for child in children:
+        if child != menu_item and isinstance(child, gtk.CheckMenuItem) and child.get_active():
+            child.set_active(False)
+
+def change_label(indicator):
+    time = datetime.datetime.now().strftime('%H:%M:%S')
+    indicator.set_label(time, '')
+    return True
 
 def quit_menu(_):
     gtk.main_quit()
-
-def set_checks(menu, menu_item):
-    children = menu.get_children()
-    for child in children:
-        label_parts = child.get_label().split(' \u2713')
-        label = label_parts[0]
-        child.set_label(label)
-        print(label)
-        if child == menu_item:
-            child.set_label(f'{label} \u2713')
-    
 
 if __name__ == '__main__':
   main()
